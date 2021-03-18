4fdd4f93d3ecb5940c139cb569699338e1a0eec0
EmperoR <lvoldemort1127@hotmail.com>
2021-03-18 16:33:07 +0800

    initial commit, create a bottom nav bar; create a nav graph

---
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -0,0 +1,37 @@
+package com.laioffer.tinnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.laioffer.tinnews.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = binding.navView;
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
+    }
+}
